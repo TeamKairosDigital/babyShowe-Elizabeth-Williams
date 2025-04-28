905a8af632a7bea106dbdb2f6d4babf5c94ca5b1
TeamKairosDigital <kairos0698@gmail.com>
2025-04-28 13:14:48 -0600

    agregando estilos de animation

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const linkElement = document.querySelector('a[href="https://maps.app.goo.gl/G4KLcWJ8sichXZwr7"]');
+    const linkElement = document.querySelector('a[href="https://maps.app.goo.gl/39E1DXqp1EnV22dFA"]');
     if (!linkElement) return;
 
     // Cambiar enlace según el dispositivo
     if (navigator.userAgent.match(/(iPhone|iPad|iPod)/i)) {
-        linkElement.href = "https://beta.maps.apple.com/?auid=10321296318776293652&lsp=9902"; // Esquema de Mapas de iOS
+        linkElement.href = "https://maps.apple.com/place?address=Avenida%2018%20Sur%20Oriente%20336,%20San%20Francisco,%2029066%20Tuxtla%20Guti%C3%A9rrez,%20Chis.,%20M%C3%A9xico&coordinate=16.738937,-93.117144&name=Kam-Fai%20Sal%C3%B3n%20%26%20Jard%C3%ADn&place-id=I8F3C9C513E353914&map=explore"; // Esquema de Mapas de iOS
     }
 });
 
@@ -35,50 +35,51 @@ simplyCountdown('#cuenta', {
     countUp: false
 });
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     var music = document.getElementById('background-music');
-//     var playButton = document.getElementById('playButton');
-//     var playIcon = playButton.querySelector('i');
-//     var contenido = document.getElementById('contenido');
+document.addEventListener('DOMContentLoaded', function() {
+    // var music = document.getElementById('background-music');
+    // var playButton = document.getElementById('playButton');
+    // var playIcon = playButton.querySelector('i');
+    // var contenido = document.getElementById('contenido');
+    // contenido.classList.add('hidden'); // Ocultar el contenido inicialmente
 
-//     // Función para alternar entre reproducción y pausa
-//     playButton.addEventListener('click', function() {
-//         if (music.paused) {
-//             // Si la música está pausada, se reproduce
-//             music.play().then(() => {
-//                 playIcon.classList.remove('fa-circle-play'); // Quita el icono de "play"
-//                 playIcon.classList.add('fa-circle-pause');  // Agrega el icono de "pause"
-//             }).catch(function(error) {
-//                 console.log('No se pudo reproducir el audio: ', error);
-//             });
-//         } else {
-//             // Si la música está en reproducción, se pausa
-//             music.pause();
-//             playIcon.classList.remove('fa-circle-pause'); // Quita el icono de "pause"
-//             playIcon.classList.add('fa-circle-play');  // Agrega el icono de "play"
-//         }
-//     });
+    // // Función para alternar entre reproducción y pausa
+    // playButton.addEventListener('click', function() {
+    //     if (music.paused) {
+    //         // Si la música está pausada, se reproduce
+    //         music.play().then(() => {
+    //             playIcon.classList.remove('fa-circle-play'); // Quita el icono de "play"
+    //             playIcon.classList.add('fa-circle-pause');  // Agrega el icono de "pause"
+    //         }).catch(function(error) {
+    //             console.log('No se pudo reproducir el audio: ', error);
+    //         });
+    //     } else {
+    //         // Si la música está en reproducción, se pausa
+    //         music.pause();
+    //         playIcon.classList.remove('fa-circle-pause'); // Quita el icono de "pause"
+    //         playIcon.classList.add('fa-circle-play');  // Agrega el icono de "play"
+    //     }
+    // });
 
-//     var modal = document.getElementById('welcomeModal');
-//     var startButton = document.getElementById('startButton');
+    // var modal = document.getElementById('welcomeModal');
+    // var startButton = document.getElementById('startButton');
 
-//     // Al hacer clic en el botón, se cierra el modal y comienza la música
-//     startButton.addEventListener('click', function() {
+    // // Al hacer clic en el botón, se cierra el modal y comienza la música
+    // startButton.addEventListener('click', function() {
 
-//         // Ocultar el modal
-//         modal.style.display = 'none';
+    //     // Ocultar el modal
+    //     modal.style.display = 'none';
 
-//         // Mostrar el contenido con efecto fade
-//         contenido.classList.remove('hidden');
+    //     // Mostrar el contenido con efecto fade
+    //     contenido.classList.remove('hidden');
 
-//         setTimeout(() => {
-//             contenido.classList.add('show'); // Activar transición
-//         }, 50); // Pequeño retraso para permitir el cambio de clase
+    //     setTimeout(() => {
+    //         contenido.classList.add('show'); // Activar transición
+    //     }, 50); // Pequeño retraso para permitir el cambio de clase
         
-//         AOS.refresh();
+    //     AOS.refresh();
 
-//         music.play().catch(function(error) {
-//             console.log('Error al reproducir el audio: ', error);
-//         });
-//     });
-// });
+    //     music.play().catch(function(error) {
+    //         console.log('Error al reproducir el audio: ', error);
+    //     });
+    // });
+});
